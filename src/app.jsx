d0b8b2f48d4641eba9c91eca441dc9aa62b30a13
AlexUrquizaa <itoliset.inc@gmail.com>
2024-06-02 13:06:37 -0300

    Se agrega el eliminar tarea

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {v4 as newTaskID}  from "uuid";
+import { v4 as newTaskID }  from "uuid";
 import './app.css';
 
 function App(){
@@ -29,20 +29,26 @@ function App(){
     setTask(prevState => prevState.map(item => item.id === task.id ? updateTask : item));
   }
 
+  const deleteTask = (task) => {
+    setTask(prevState => prevState.filter(item => item.id !== task.id));
+  }
+
   return (
     <>
+      <h3>Crear listado tareas</h3>
+
       <form onSubmit={handleClick}>
         <input type='text' placeholder='Ingresar titulo tarea' onChange={handleInputTask} value={title}/>
         <button type='submit'>Agregar</button>
       </form>
 
-      { tasks.length === 0 ? ( <p>No hay tareas disponibles</p>) : 
+      { tasks.length === 0 ? (<p>No hay tareas disponibles</p>) : 
         (
           tasks.map(task => (
             <article key={task.id}>
               <input type='checkbox' onChange={() => handleChecked(task)} checked={task.isCompleted}/>
               <span className={task.isCompleted ? "completed" : ""}>{task.title}</span>
-              <button>Eliminar</button>
+              <button onClick={() => deleteTask(task)}>Eliminar</button>
             </article>
           ))
         )
