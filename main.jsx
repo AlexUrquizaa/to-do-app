9c7fd3acfcfce9bdedd1a69226e6e5f05f617b84
AlexUrquizaa <itoliset.inc@gmail.com>
2024-06-02 01:49:06 -0300

    Pasa modo StrictMode

--- main.jsx
+++ main.jsx
@@ -1,8 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./src/app";
 import './index.css';
+import { StrictMode } from "react";
 
 const root = createRoot(document.getElementById('app'));
 root.render(
-  <App />
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
