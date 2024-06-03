89ecf5210da3060f4a711057fa2101384fcacddb
AlexUrquizaa <itoliset.inc@gmail.com>
2024-06-02 21:55:33 -0300

    Se simplifica codigo

--- src/app.jsx
+++ src/app.jsx
@@ -1,58 +1,28 @@
-import { useState } from "react";
-import { v4 as newTaskID }  from "uuid";
+import { useTaskList } from "./hooks/useTaskList";
+import { ShowTaskList } from "./componentes/ShowTaskList";
+import { TaskForm } from "./componentes/TaskForm";
 import './app.css';
 
 function App(){
-  const [title, setTitle] = useState('');
-  const [tasks, setTask] = useState([]);
+  const { tasks, addTask, updateTask, removeTask} = useTaskList();
 
-  const handleInputTask = (event) => {
-    const value = event.target.value;
-    setTitle(value);
+  const handleSubmit = (title) => {
+    addTask(title);
   }
 
-  const handleClick = (event) => {
-    event.preventDefault();
-
-    setTask(prevState => ([
-      ...prevState,
-      {
-        title: title, 
-        id: newTaskID(), 
-        isCompleted: false
-      }
-    ]));
+  const handleCheck = (id) => {
+    updateTask(id);
   }
 
-  const handleChecked = (task) => {
-    const updateTask = {...task, isCompleted: !task.isCompleted};
-    setTask(prevState => prevState.map(item => item.id === task.id ? updateTask : item));
-  }
-
-  const deleteTask = (task) => {
-    setTask(prevState => prevState.filter(item => item.id !== task.id));
+  const handleRemove = (id) => {
+    removeTask(id);
   }
 
   return (
     <>
-      <h3>Crear listado tareas</h3>
-
-      <form onSubmit={handleClick}>
-        <input type='text' placeholder='Ingresar titulo tarea' onChange={handleInputTask} value={title}/>
-        <button type='submit'>Agregar</button>
-      </form>
-
-      { tasks.length === 0 ? (<p>No hay tareas disponibles</p>) : 
-        (
-          tasks.map(task => (
-            <article key={task.id}>
-              <input type='checkbox' onChange={() => handleChecked(task)} checked={task.isCompleted}/>
-              <span className={task.isCompleted ? "completed" : ""}>{task.title}</span>
-              <button onClick={() => deleteTask(task)}>Eliminar</button>
-            </article>
-          ))
-        )
-      }
+      <h3>Crear listado tareas</h3><br/>
+      <TaskForm getNewTask={handleSubmit}/>
+      <ShowTaskList tasks={tasks} update={handleCheck} remove={handleRemove}/>
     </>
   );
 }
