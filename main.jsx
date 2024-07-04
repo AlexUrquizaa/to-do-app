ef74660573d620ff81c57d51e9c87ef8c840d1b1
AlexUrquizaa <itoliset.inc@gmail.com>
2024-07-04 02:28:28 -0300

    Modifico nombre filterContext.jsx a FilterContext.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./src/app";
 import './index.css';
-import { FiltersProvider } from "./src/componentes/compsDeContexto/filterContext";
+import { FiltersProvider } from "./src/componentes/compsDeContexto/FilterContext";
 
 const root = createRoot(document.getElementById('app'));
 root.render(
--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+// import { FiltersContext } from "../componentes/compsDeContexto/FilterContext";
 import { FiltersContext } from "../componentes/compsDeContexto/FilterContext";
 
 export function useFilter(){
