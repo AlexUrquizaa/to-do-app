12d3cb2830900377a1b1d6635c3067660d2800d2
AlexUrquizaa <itoliset.inc@gmail.com>
2024-07-15 14:14:52 -0300

    Muevo las acciones de filter al context

--- src/componentes/compsDeContexto/filterContext.jsx
+++ src/componentes/compsDeContexto/filterContext.jsx
@@ -3,10 +3,16 @@ import { useState, createContext } from "react";
 export const FiltersContext = createContext();
 export function FiltersProvider({ children }){
   const [ filters, setFilters ] = useState('');
+
+  const filterTasks = ( tasks ) => {
+    return tasks.filter(task => task.title.toLowerCase().startsWith(filters.toLowerCase()));
+  }
+
   return(
     <FiltersContext.Provider value={{
       filters,
-      setFilters
+      setFilters,
+      filterTasks
     }}>
       {children}
     </FiltersContext.Provider>
--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -3,13 +3,6 @@ import { FiltersContext } from "../componentes/compsDeContexto/FilterContext";
 
 export function useFilter(){
   const context = useContext(FiltersContext);
-  const { filters, setFilters } = useContext(FiltersContext);
-
-  if(context === undefined) throw new Error('No esta envuelto en su contexto');
-
-  const filterTasks = ( tasks ) => {
-    return tasks.filter(task => task.title.toLowerCase().startsWith(filters.toLowerCase()));
-  }
-
-  return { filterTasks, setFilters };
+  if(context === undefined) throw new Error('No esta envuelto en su contexto de Filters');
+  return context;
 }
