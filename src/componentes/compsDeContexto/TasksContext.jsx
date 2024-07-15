f286cfb8c2c78628bc7d52902dceb7bd06d31c06
AlexUrquizaa <itoliset.inc@gmail.com>
2024-07-15 14:10:42 -0300

    Muevo las acciones de las task al context

--- src/componentes/compsDeContexto/TasksContext.jsx
+++ src/componentes/compsDeContexto/TasksContext.jsx
@@ -1,11 +1,34 @@
 import { useState, createContext } from "react";
+import { v4 as newTaskID }  from "uuid";
 
 export const TasksContext = createContext();
 export function TasksProvider({ children }){
   const [ tasks, setTasks ] = useState([]);
+
+  const addTask = (newTitle) => {
+    setTasks(prevState => [
+      ...prevState,
+      {
+        title: newTitle,
+        id: newTaskID(),
+        isCompleted: false
+      }
+    ]);
+  }
+
+  const updateTask = (id) => {
+    const newTasks = tasks.map(task => task.id === id ? {...task, isCompleted: !task.isCompleted} : task);
+    setTasks(newTasks);
+  }
+
+  const removeTask = (id) => {
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks(newTasks);
+  }
+
   return (
     <TasksContext.Provider value={{
-      tasks, setTasks
+      tasks, setTasks, addTask, updateTask, removeTask
     }}>
       {children}
     </TasksContext.Provider>
--- src/hooks/useTask.js
+++ src/hooks/useTask.js
@@ -1,33 +1,10 @@
 import { useContext } from "react";
 import { TasksContext } from "../componentes/compsDeContexto/TasksContext";
-import { v4 as newTaskID }  from "uuid";
 
 export function useTask(){
   const context = useContext(TasksContext);
-  const { tasks, setTasks } = useContext(TasksContext);
 
   if(context === undefined) throw new Error('No esta envuelto en su contexto de Tasks');
 
-  const addTask = (newTitle) => {
-    setTasks(prevState => [
-      ...prevState,
-      {
-        title: newTitle,
-        id: newTaskID(),
-        isCompleted: false
-      }
-    ]);
-  }
-
-  const updateTask = (id) => {
-    const newTasks = tasks.map(task => task.id === id ? {...task, isCompleted: !task.isCompleted} : task);
-    setTasks(newTasks);
-  }
-
-  const removeTask = (id) => {
-    const newTasks = tasks.filter(task => task.id !== id);
-    setTasks(newTasks);
-  }
-
-  return { addTask, updateTask, removeTask, tasks };
+  return context;
 }
